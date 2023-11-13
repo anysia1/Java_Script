e06bbbd411874ea81ce920be7fa57370844e7231
anysia1 <anysiakamanzi@gmail.com>
2023-11-13 18:02:56 +0200

    javascript assignment

---
+++ Qn10.js
@@ -0,0 +1,10 @@
+function displayPattern(rows, columns) {
+  for (let i = 0; i < rows; i++) {
+    let row = '';
+    for (let j = 0; j < columns; j++) {
+      row += '* ';
+    }
+    console.log(row);
+  }
+}
+displayPattern(4, 5);
---
+++ product.js
@@ -0,0 +1,6 @@
+function assignment(a, b){
+var product = a * b;
+return product;
+}
+var result = assignment(3, 6);
+console.log(result)
